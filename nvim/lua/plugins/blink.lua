603da5a39bd5d3f316dd0d17a33107e86848b66d
Caolin Hughes <caolinhughes@gmail.com>
2025-08-20 00:00:07 +0100

    feat: set documentation to show for completion

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -1,21 +1,26 @@
 local M = {
-    "saghen/blink.cmp",
-    dependencies = {
-        { "rafamadriz/friendly-snippets" },
-        { "L3MON4D3/LuaSnip", version = "v2.*" },
-    },
-    version = "1.*",
+	"saghen/blink.cmp",
+	dependencies = {
+		{ "rafamadriz/friendly-snippets" },
+		{ "L3MON4D3/LuaSnip", version = "v2.*" },
+	},
+	version = "1.*",
 
-    opts = {
-        keymap = {
-            preset = "none",
-            
-            ["<CR>"] = { "accept", "fallback" },
-            ["<Tab>"] = { "select_next", "fallback" },
-            ["<S-Tab>"] = { "select_prev", "fallback" },
-        },
-    },
-    opts_extend = { "sources.default" }
+	opts = {
+		completion = {
+			documentation = {
+				auto_show = true,
+			},
+		},
+		keymap = {
+			preset = "none",
+
+			["<CR>"] = { "accept", "fallback" },
+			["<Tab>"] = { "select_next", "fallback" },
+			["<S-Tab>"] = { "select_prev", "fallback" },
+		},
+	},
+	opts_extend = { "sources.default" },
 }
 
 return M
