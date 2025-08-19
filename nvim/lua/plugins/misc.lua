6cf769feca65a5a8c283d762a9643760947787eb
Caolin Hughes <caolinhughes@gmail.com>
2025-08-19 21:24:14 +0100

    feat: add small miscellaneous plugins

---
+++ nvim/lua/plugins/misc.lua
@@ -0,0 +1,13 @@
+local M = {
+    { "nvim-tree/nvim-web-devicons", opts = { default = true } },
+
+    { "windwp/nvim-ts-autotag", config = true },
+
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        main = "ibl",
+        config = true,
+    },
+}
+
+return M
