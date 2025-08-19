688e01746c258d14852c7a47b6fd797586ac45ad
Caolin Hughes <caolinhughes@gmail.com>
2025-08-19 21:23:33 +0100

    feat: add lualine statusline

---
+++ nvim/lua/plugins/lualine.lua
@@ -0,0 +1,16 @@
+local M = {
+    "nvim-lualine/lualine.nvim",
+
+    opts = {
+        theme = "gruvbox",
+        sections = {
+            lualine_x = {
+                "os.date('%H:%M', os.time())",
+                "fileformat",
+                "filetype"
+            }
+        }
+    },
+}
+
+return M
