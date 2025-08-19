1419bd949846cf937ae55816530d9c231dbf5af0
Caolin Hughes <caolinhughes@gmail.com>
2025-08-19 21:39:01 +0100

    feat: add setting to lua_ls to remove annoying warning

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -3,6 +3,16 @@ local M = {
 
     config = function()
         vim.lsp.config("*", {})
+        vim.lsp.config("lua_ls", {
+            settings = {
+                Lua = {
+                    diagnostics = {
+                        -- remove annoying undefined global vim warning
+                        globals = { "vim", },
+                    },
+                },
+            },
+        })
 
         vim.diagnostic.config({
             virtual_lines = {
