12e59b74b41f47d0d8c3984af730d66d81c16337
Caolin Hughes <caolinhughes@gmail.com>
2025-08-20 22:30:27 +0100

    feat: change basedpyright default diagnostic mode

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,44 +1,53 @@
 local M = {
-    "neovim/nvim-lspconfig",
+	"neovim/nvim-lspconfig",
 
-    config = function()
-        vim.lsp.config("*", {})
-        vim.lsp.config("lua_ls", {
-            settings = {
-                Lua = {
-                    diagnostics = {
-                        -- remove annoying undefined global vim warning
-                        globals = { "vim", },
-                    },
-                },
-            },
-        })
+	config = function()
+		vim.lsp.config("*", {})
+		vim.lsp.config("basedpyright", {
+			settings = {
+				basedpyright = {
+					analysis = {
+						diagnosticMode = "workspace",
+					},
+				},
+			},
+		})
+		vim.lsp.config("lua_ls", {
+			settings = {
+				Lua = {
+					diagnostics = {
+						-- remove annoying undefined global vim warning
+						globals = { "vim" },
+					},
+				},
+			},
+		})
 
-        vim.diagnostic.config({
-            priority = {
-                [vim.diagnostic.severity.ERROR] = 3,
-                [vim.diagnostic.severity.WARN] = 2,
-                [vim.diagnostic.severity.INFO] = 1,
-                [vim.diagnostic.severity.HINT] = 0,
-            },
-            severity_sort = true,
-            signs = {
-                text = {
-                    [vim.diagnostic.severity.ERROR] = "",
-                    [vim.diagnostic.severity.WARN] = "",
-                    [vim.diagnostic.severity.INFO] = "",
-                    [vim.diagnostic.severity.HINT] = "",
-                }
-            },
-            virtual_lines = {
-                current_line = true,
-            },
-        })
-        vim.lsp.enable({
-            "basedpyright",
-            "lua_ls",
-        })
-    end,
+		vim.diagnostic.config({
+			priority = {
+				[vim.diagnostic.severity.ERROR] = 3,
+				[vim.diagnostic.severity.WARN] = 2,
+				[vim.diagnostic.severity.INFO] = 1,
+				[vim.diagnostic.severity.HINT] = 0,
+			},
+			severity_sort = true,
+			signs = {
+				text = {
+					[vim.diagnostic.severity.ERROR] = "",
+					[vim.diagnostic.severity.WARN] = "",
+					[vim.diagnostic.severity.INFO] = "",
+					[vim.diagnostic.severity.HINT] = "",
+				},
+			},
+			virtual_lines = {
+				current_line = true,
+			},
+		})
+		vim.lsp.enable({
+			"basedpyright",
+			"lua_ls",
+		})
+	end,
 }
 
 return M
