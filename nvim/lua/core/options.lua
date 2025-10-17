5cf0c5fc7e02a1f520615a1930233b0c893a0088
Caolin Hughes <caolinhughes@gmail.com>
2025-10-17 14:39:55 +0100

    feat: change tabs to 2 spaces for eslint languages

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -7,6 +7,24 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.bo.softtabstop = 4
 
+-- set tabs to 2 spaces for eslint recommended languages
+vim.api.nvim_create_autocmd({ "FileType" }, {
+	pattern = {
+		"typescript",
+		"typescriptreact",
+		"javascript",
+		"javascriptreact",
+		"json",
+		"jsonc",
+	},
+	callback = function()
+		vim.opt_local.tabstop = 2
+		vim.opt_local.shiftwidth = 2
+		vim.opt_local.expandtab = true
+		vim.opt_local.softtabstop = 2
+	end,
+})
+
 -- line numbers
 vim.opt.number = true
 vim.opt.relativenumber = true
